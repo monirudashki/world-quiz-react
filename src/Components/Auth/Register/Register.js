91edb21e7543b441600965c00ca4d422f9db7836
simeon.rudashki <simeon.rudashki@abv.bg>
2023-02-01 19:42:16 +0200

    Work with database , registered user

--- src/Components/Auth/Register/Register.js
+++ src/Components/Auth/Register/Register.js
@@ -1,29 +1,151 @@
 import styles from '../Register/Register.module.css';
 
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react';
+
+const apiUrl = "http://localhost:3030/api";
+
 export const Register = () => {
+
+    const navigate = useNavigate();
+
+    const [error, setError] = useState('');
+
+    const [errors, setErrors] = useState({});
+
+    const [formValues, setFormValues] = useState({
+        username: '',
+        email: '',
+        imageUrl: '',
+        password: '',
+        rePass: ''
+    });
+
+    const onChangeValueHandler = (e) => {
+        setFormValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const minLength = (e, min) => {
+        setErrors(state => ({
+            ...state,
+            [e.target.name]: formValues[e.target.name].length < min
+        }))
+    }
+
+    const emailValidator = () => {
+        const pattern = /^.{3,}@gmail\.(bg|com)$/;
+
+        setErrors(state => ({
+            ...state,
+            email: !pattern.test(formValues.email)
+        }))
+    }
+
+    const imageUrlValidator = () => {
+        setErrors(state => ({
+            ...state,
+            imageUrl: !formValues.imageUrl.startsWith('http') || !formValues.imageUrl.startsWith('https')
+        }))
+    }
+
+    const passwordsMatch = () => {
+        setErrors(state => ({
+            ...state,
+            rePass: !(formValues.password === formValues.rePass)
+        }))
+    }
+
+    const invalidForm = Object.values(formValues).some(x => x);
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        console.log('submit')
+
+        const userData = { ...formValues };
+
+        const res = await fetch(`${apiUrl}/register`, {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userData)
+        });
+
+        const result = await res.json();
+
+        console.log(result);
+
+        if (res.ok === true) {
+            navigate('/');
+        } else {
+            setError(error => result.message);
+        }
+
+    }
+
     return (
         <section id="register">
             <div className={styles["form"]}>
                 <h2>Registration</h2>
 
-                <div className={styles["error"]}>
-                </div>
-
-                <form className={styles["login-form"]}>
-                    <input type="text" name="username" id="register-username" placeholder="username" />
-
-                    <input type="text" name="email" id="register-email" placeholder="email" />
-
-                    <div formGroupName="passwords">
-                        <input type="password" name="password" id="register-password" placeholder="password" />
-
-
-
-                        <input type="password" name="re-password" id="repeat-password" placeholder="repeat password" />
+                {error &&
+                    <div className={styles["error"]}>
+                        {error}
                     </div>
+                }
 
-                    <button type="submit">Register</button>
-                    <p className={styles["message"]}>Already registered? <a href='#/'>Login</a></p>
+                <form className={styles["login-form"]} onSubmit={onSubmitHandler}>
+
+                    <label htmlFor="register-username">Username: </label>
+                    <input type="text" name="username" id="register-username" placeholder="username"
+                        value={formValues.username} onChange={onChangeValueHandler} onBlur={(e) => { minLength(e, 3) }}
+                    />
+
+                    {errors.username &&
+                        <p className={styles['error']}>Username must be at least 3 characters long!</p>
+                    }
+
+                    <label htmlFor="register-email">Email: </label>
+                    <input type="text" name="email" id="register-email" placeholder="email"
+                        value={formValues.email} onChange={onChangeValueHandler} onBlur={emailValidator}
+                    />
+
+                    {errors.email &&
+                        <p className={styles['error']}>Email must be valid!</p>
+                    }
+
+                    <label htmlFor="register-imageUrl">ImageUrl: </label>
+                    <input type="text" name="imageUrl" id="register-imageUrl" placeholder="image Url"
+                        value={formValues.imageUrl} onChange={onChangeValueHandler} onBlur={imageUrlValidator}
+                    />
+
+                    {errors.imageUrl &&
+                        <p className={styles['error']}>ImageUrl must be a valid link!</p>
+                    }
+
+
+                    <label htmlFor="register-password">Password: </label>
+                    <input type="password" name="password" id="register-password" placeholder="password"
+                        value={formValues.password} onChange={onChangeValueHandler} onBlur={(e) => { minLength(e, 5) }}
+                    />
+
+                    {errors.password &&
+                        <p className={styles['error']}>Password must be at least 5 characters long!</p>
+                    }
+
+                    <label htmlFor="repeat-password">Confirm Password: </label>
+                    <input type="password" name="rePass" id="repeat-password" placeholder="repeat password"
+                        value={formValues.rePass} onChange={onChangeValueHandler} onBlur={passwordsMatch}
+                    />
+
+                    {errors.rePass &&
+                        <p className={styles['error']}>Passwords don't match!</p>
+                    }
+
+                    <button id='action-save' type="submit" disabled={!invalidForm}>Register</button>
+                    <p className={styles["message"]}>Already registered? <Link to="/auth/login">Login</Link></p>
                 </form>
             </div>
         </section>
--- src/Components/Core/Header/Header.js
+++ src/Components/Core/Header/Header.js
@@ -1,86 +1,51 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+
 import image from '../../../Assets/images/logo-1.jpg';
 import styles from '../Header/Header.module.css';
 
 function Header() {
 
-    //TODO optimization of active buttons?
+    //navigate on page correct works?
 
     const [currentUser, setCurrentUser] = useState('admin');
 
-    const [activePage, setActivePage] = useState('');
-
-    const activeHome = () => {
-        setActivePage(oldPage => '');
-    }
-
-    const activeCapitals = () => {
-        setActivePage(oldPage => 'capitals');
-    }
-    const activeFlags = () => {
-        setActivePage(oldPage => 'flags');
-    }
-    const activeAddCapitals = () => {
-        setActivePage(oldPage => 'addCapitals');
-    }
-    const activeAddFlags = () => {
-        setActivePage(oldPage => 'addFlags');
-    }
-
-    const activeLogin = () => {
-        setActivePage(oldPage => 'login');
-        // setCurrentUser(user => 'user');
-    }
-
-    const activeRegister = () => {
-        setActivePage(oldPage => 'register');
-        // setCurrentUser(user => 'admin');
-    }
-
-    const activeRules = () => {
-        setActivePage(oldPage => 'rules');
-    }
-
-    const activeScoreBoard = () => {
-        setActivePage(oldPage => 'scoreBoard');
-    }
-
-    const activeProfile = () => {
-        setActivePage(oldPage => 'profile');
+    const setActiveStyle = ({ isActive }) => {
+        return isActive
+            ? styles['active']
+            : 'none'
     }
 
     const logoutHandler = () => {
         setCurrentUser(user => null);
-        setActivePage(oldPage => '');
     }
 
     return (
         <header className={styles['header']}>
-            <Link onClick={activeHome} className={styles["logo"]} to="/"><img className={styles["logo-img"]} src={image} alt="" /></Link>
+            <NavLink className={styles["logo"]} to="/"><img className={styles["logo-img"]} src={image} alt="" /></NavLink>
             <nav className={styles['nav']}>
 
                 {currentUser === null &&
                     <>
-                        <Link to="/auth/login" onClick={activeLogin} className={activePage == 'login' ? styles['active'] : 'none'}>Login</Link>
-                        <Link to="/auth/register" onClick={activeRegister} className={activePage == 'register' ? styles['active'] : 'none'}>Register</Link>
+                        <NavLink to="/auth/login" className={setActiveStyle}>Login</NavLink>
+                        <NavLink to="/auth/register" className={setActiveStyle}>Register</NavLink>
                     </>
                 }
 
                 {currentUser === 'admin' &&
                     <>
-                        <Link to={'/admin/capitals-questions'} onClick={activeCapitals} className={activePage == 'capitals' ? styles['active'] : 'none'}>Capitols</Link>
-                        <Link to={'/admin/flags-questions'} onClick={activeFlags} className={activePage == 'flags' ? styles['active'] : 'none'}>Flags</Link>
-                        <Link to={'/admin/add-capitals-question'} onClick={activeAddCapitals} className={activePage == 'addCapitals' ? styles['active'] : 'none'}>Add Capitals</Link>
-                        <Link to={'/admin/add-flags-question'} onClick={activeAddFlags} className={activePage == 'addFlags' ? styles['active'] : 'none'}>Add Flags</Link>
+                        <NavLink to={'/admin/capitals-questions'} className={setActiveStyle}>Capitols</NavLink>
+                        <NavLink to={'/admin/flags-questions'} className={setActiveStyle}>Flags</NavLink>
+                        <NavLink to={'/admin/add-capitals-question'} className={setActiveStyle}>Add Capitals</NavLink>
+                        <NavLink to={'/admin/add-flags-question'} className={setActiveStyle}>Add Flags</NavLink>
                         <a onClick={logoutHandler}>Logout</a>
                     </>
                 }
                 {currentUser === 'user' &&
                     <>
-                        <Link to={'/rules'} onClick={activeRules} className={activePage == 'rules' ? styles['active'] : 'none'}>Rules</Link>
-                        <Link to={'/scoreboard'} onClick={activeScoreBoard} className={activePage == 'scoreBoard' ? styles['active'] : 'none'}>Scoreboard</Link>
-                        <Link to={'/auth/user-profile/rudashki'} onClick={activeProfile} className={activePage == 'profile' ? styles['active'] : 'none'}>Rudashki</Link>
+                        <Link to={'/rules'} className={setActiveStyle}>Rules</Link>
+                        <Link to={'/scoreboard'} className={setActiveStyle}>Scoreboard</Link>
+                        <Link to={'/auth/user-profile/rudashki'} className={setActiveStyle}>Rudashki</Link>
                         <a onClick={logoutHandler}>Logout</a>
                     </>
                 }
